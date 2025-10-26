4156ac2a65714c3033dfadf1729ca406c9e6f15c
Meghana Poojary <meghanapoojary121@gmail.com>
2025-10-26 22:13:27 +0530

    Hosting error resolved

--- src/aws/admin-config.js
+++ src/aws/admin-config.js
@@ -1,10 +1,19 @@
+// export const adminAmplifyConfig = {
+//   Auth: {
+//     Cognito: {
+//       userPoolId: import.meta.env.VITE_ADMIN_POOL_ID, 
+//       userPoolClientId: import.meta.env.VITE_ADMIN_CLIENT_ID, 
+//       signUpVerificationMethod: "code",
+//       loginWith: { username: false, email: true },
+//     },
+//   },
+// };
+
 export const adminAmplifyConfig = {
   Auth: {
-    Cognito: {
-      userPoolId: import.meta.env.VITE_ADMIN_POOL_ID, 
-      userPoolClientId: import.meta.env.VITE_ADMIN_CLIENT_ID, 
-      signUpVerificationMethod: "code",
-      loginWith: { username: false, email: true },
-    },
+    userPoolId: import.meta.env.VITE_ADMIN_POOL_ID, 
+    userPoolClientId: import.meta.env.VITE_ADMIN_CLIENT_ID, 
+    region: us-east-1, // you must specify the region
+    authenticationFlowType: "USER_PASSWORD_AUTH",
   },
 };
--- src/aws/student-config.js
+++ src/aws/student-config.js
@@ -1,10 +1,19 @@
+// export const studentAmplifyConfig = {
+//   Auth: {
+//     Cognito: {
+//       userPoolId: import.meta.env.VITE_STUDENT_POOL_ID, 
+//       userPoolClientId: import.meta.env.VITE_STUDENT_CLIENT_ID, 
+//       signUpVerificationMethod: "code",
+//       loginWith: { username: false, email: true },
+//     },
+//   },
+// };
+
 export const studentAmplifyConfig = {
   Auth: {
-    Cognito: {
-      userPoolId: import.meta.env.VITE_STUDENT_POOL_ID, 
-      userPoolClientId: import.meta.env.VITE_STUDENT_CLIENT_ID, 
-      signUpVerificationMethod: "code",
-      loginWith: { username: false, email: true },
-    },
+    userPoolId: import.meta.env.VITE_STUDENT_POOL_ID, 
+    userPoolClientId: import.meta.env.VITE_STUDENT_CLIENT_ID, 
+    region: us-east-1, // you must specify the region
+    authenticationFlowType: "USER_PASSWORD_AUTH",
   },
 };
